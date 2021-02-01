65c6ea3b3e2d3af3866db0716a3876ff9b0b8212
jimwhitfield <jim.whitfield@gmail.com>
2021-02-01 14:59:36 -0800

    Follow guidance from github.com/serialport/node-serialport

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ if (process.argv.length >= 3) {
 	launch(portParam,debug);
 } else {
 	var SerialPort = require('serialport');
-	SerialPort.list(function (err, ports) {
+	//was: SerialPort.list(function (err, ports) {
+	SerialPort.list().then(ports => {
 	  if (ports.length == 1) {
 	    console.log("Usage: " + __filename + " PORT\nProceeding with only active port "+ports[0].comName+"\n");
 	  	launch(ports[0].comName,debug);
